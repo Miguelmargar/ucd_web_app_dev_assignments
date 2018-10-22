b3f3d816eeb754b9e32c47876547a7ff1b555147
Miguel Martinez <miguelitalia@hotmail.com>
2018-10-22 17:49:46 +0000

    working on displaying the correct nobel winners with dates given

--- assignment2/nobels.js
+++ assignment2/nobels.js
@@ -1,20 +1,38 @@
-var xmlhttp = new XMLHttpRequest();
-var url = "nobels.json";
-
-xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var myArr = JSON.parse(xmlhttp.responseText);
-        myFunction(myArr)
+function display() {
+    
+    var xmlhttp = new XMLHttpRequest();
+    var url = "nobels.json";
+    
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var myArr = JSON.parse(xmlhttp.responseText);
+            displayJSON(myArr);
+        }
+    };
+    
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    
+    var info1 = document.getElementById("year1").innerText;    
+    var info2 = document.getElementById("info_needed").elements[1].value;
+    var info3 = document.getElementById("info_needed").elements[2].value;
+    var info4 = document.getElementById("info_needed").elements[3].value;
+    
+    function displayJSON(obj) {
+        var nobels = obj.laureates;
+        var firstName = nobels.firstname;
+        var surname = nobels.surname;
+        var countryBirth = nobels.bornCountry;
+        var yearPrize = nobels.prizes.year;
+        var category = nobels.prizes.category;
+        
+        
+        
+        for (var i = 0; i < obj.length; i++) {
+            document.getElementById("id01").innerHTML = obj.laureates.firstname[i];     
+        }
+        
     }
-};
-
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-function myFunction(arr) {
-    var out = "";
-    for (var i = 0; i < arr.length; i++) {
-        out += '<a href="' + arr[i].url + '">' + arr[i].display + "</a><br>";
-    }
-    document.getElementById("id01").innerHTML = out;
 }
+
+
