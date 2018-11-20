dcf1b64a0e3fa0fd6c0a761b3aff88b96b636886
Miguel Martinez <miguelitalia@hotmail.com>
2018-11-20 16:44:48 +0000

    added code to index.php showing productLines and descriptions as table

--- assignment3/index.php
+++ assignment3/index.php
@@ -13,20 +13,56 @@
     
     <main>
         <?php
+        echo "<table>";
+        echo "<tr><th>Product Line</th><th>Description</th></tr>";
+        
+        class TableRows extends RecursiveIteratorIterator { 
+            function __construct($it) { 
+                parent::__construct($it, self::LEAVES_ONLY); 
+            }
+        
+            function current() {
+                return "<td>" . parent::current(). "</td>";
+            }
+            
+            function beginChildren() { 
+                echo "<tr>"; 
+            }
+            
+            function endChildren() { 
+                echo "</tr>" . "\n";
+            }
+        }
+        
+        
         $servername = "localhost";
         $username = "root";
         $password = "";
+        $dbname = "classicmodels";
 
         try {
-            $conn = new PDO("mysql:host=$servername;dbname=classicmodels", $username, $password);
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo "Connected successfully"; 
+            $stmt = $conn->prepare("SELECT productLine, textDescription FROM productlines");
+            $stmt->execute();
+            
+            // set the resulting array to associative
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            
+            foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+                echo $v;
             }
+            
+        }
+            
         catch(PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
-            }
-    
+        }
+        
+        $conn = null;
+        echo "</table>";
         ?>
     </main>
     
