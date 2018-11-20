47f582aa3798eb883e9997d74e830a8bfdb91b33
Miguel Martinez <miguelitalia@hotmail.com>
2018-11-20 18:15:07 +0000

    added code to payments.php to show database details

--- assignment3/payments.php
+++ assignment3/payments.php
@@ -12,7 +12,58 @@
     </header>
     
     <main>
+        <?php
+        echo "<table>";
+        echo "<tr><th>Customer Number</th><th>Check Number</th><th>Payment Date</th><th>Amount</th></tr>";
         
+        class TableRows extends RecursiveIteratorIterator { 
+            function __construct($it) { 
+                parent::__construct($it, self::LEAVES_ONLY); 
+            }
+        
+            function current() {
+                return "<td>" . parent::current(). "</td>";
+            }
+            
+            function beginChildren() { 
+                echo "<tr>"; 
+            }
+            
+            function endChildren() { 
+                echo "</tr>" . "\n";
+            }
+        }
+        
+        
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "classicmodels";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully"; 
+            $stmt = $conn->prepare("SELECT customerNumber, checkNumber, paymentDate, amount  FROM payments");
+            $stmt->execute();
+            
+            // set the resulting array to associative
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            
+            foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+                echo $v;
+            }
+            
+        }
+            
+        catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+        
+        $conn = null;
+        echo "</table>";
+        ?>
     </main>
     
     <footer>
